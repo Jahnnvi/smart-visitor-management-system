49e55028da4e6183bbd3efa763b23d1c0b6c83df
mitali-7 <mitalig2107@gmail.com>
2026-02-09 19:11:45 +0530

    Invite guest request logs

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AdminLogin from "./Pages/AdminLogin";
 import SecurityLogin from "./Pages/SecurityLogin";
 import GuestRequest from "./Pages/guest/GuestRequest";
 import GuestPage from "./Pages/GuestPage";
-// import GuestStatus from "./Pages/guest/GuestStatus";
+import GuestStatus from "./Pages/guest/GuestStatus";
 import AdminPage from "./Pages/AdminPage";
 import PendingRequest from "./Pages/admin/PendingRequest";
 import AssignAttendee from "./Pages/admin/AssignAttendee";
@@ -30,7 +30,7 @@ function App() {
         <Route path="/admin/logs" element={<VisitorLogs />} />
         <Route path="/guest" element={<GuestPage />} />
         <Route path="/guest/request" element={<GuestRequest />} />
-        {/* <Route path="/guest/status" element={<GuestStatus />} /> */}
+        <Route path="/guest/status" element={<GuestStatus />} />
         <Route path="/guest/login" element={<GuestLogin />} />
         <Route path="/security/login" element={<SecurityLogin />} />
         <Route path="/security/dashboard" element={<GateDashboard />} />
